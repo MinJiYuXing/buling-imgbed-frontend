28d3ed42db732c4b2c871d8fbb4d6b1882d1ebe2
李铭 <38746076+MinJiYuXing@users.noreply.github.com>
2024-11-20 00:20:22 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,10 +24,10 @@ export default defineNuxtConfig({
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { name: 'description', content: '布灵图床 - 基于 Cloudflare 全家桶(Workers/Pages/D1/R2)打造的简单好用的个人图片托管解决方案' },
+                { name: 'description', content: 'TG布灵图床 - 基于 Cloudflare 全家桶(Workers/Pages/D1/R2)打造的简单好用的个人图片托管解决方案' },
                 { name: 'keywords', content: '图床,Cloudflare,Workers,Pages,D1,R2,图片托管,图片上传,免费图床' },
-                { name: 'author', content: '优优Anuuu.com' },
-                { property: 'og:title', content: '布灵图床 - 简单好用的个人图片托管方案' },
+                { name: 'author', content: 'vocal.us.kg' },
+                { property: 'og:title', content: 'TG图床 - 简单好用的个人图片托管方案' },
                 { property: 'og:description', content: '基于 Cloudflare 全家桶(Workers/Pages/D1/R2)打造的简单好用的个人图片托管解决方案' },
                 { property: 'og:type', content: 'website' },
                 { name: 'robots', content: 'index,follow' }
